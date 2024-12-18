795426ff852a06f7c5aff3142aeda38c6af6dfd9
Ashley Wilkey <contact@ashleywilkey.com>
2024-12-18 10:40:31 -0800

    practice bind assignment

--- employee.js
+++ employee.js
@@ -1,1 +1,15 @@
 // Your code here
+export class Employee {
+  constructor(name, occupation) {
+    this.name = name;
+    this.occupation = occupation;
+  }
+
+  sayName() {
+    console.log(`${this.name} says hello.`);
+  }
+
+  sayOccupation() {
+    console.log(`${this.name} is a ${this.occupation}`);
+  }
+}
--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 // Your code here
+import { Employee } from "./employee.js";
+
+const john = new Employee("John Wick", "Dog Lover");
+john.sayName();
+setTimeout(john.sayName.bind(john), 2 * 1000);
+setTimeout(john.sayOccupation.bind(john), 3000);
